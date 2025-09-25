17d2beb1ac0fec4f98d3c3c0de7498310a607051
Felipe Balbino Murad <felipebmurad@gmail.com>
2025-09-25 11:16:09 -0300

    Update EX.py

--- Semestre 2/EX.py
+++ Semestre 2/EX.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import requests
 
 acougue = {
     'Carnes' : ['Patinho', 'Coxão Mole','Fraldinha', 'Picanha', 'Maminha', 'Linguiça'],
@@ -23,23 +23,36 @@ def forca_opcao(msg, lista_opcoes):
     return opcao
 
 
+def cadastro_endereco():
+    while True:
+        cep = input('Diga o seu CEP:\n-> ')
+        endereco = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+        if endereco.status_code ==200:
+            carrinho['Endereço']= endereco.json()
+            carrinho['Endereço']['Nª'] = input('Numero da residencia: \n->')
+            carrinho['Endereço']['Complemento'] = input('Complemento: \n ->')
+
 def add_prod():
+    global indices
     for key in acougue.keys():
         info = input(f'Diga o novo {key}: ')
         acougue[key].append(info)
     print(pd.DataFrame(acougue))
+    indices = cria_indices()
     return
 
 def rem_prod():
+    global indices
     item = forca_opcao("Qual carne vc quer remover?", acougue['Carnes'])
     indice_item = indices[item]
     for key in acougue.keys():
         acougue[key].pop(indice_item)
     print(pd.DataFrame(acougue))
+    indices = cria_indices()
     return
 
 def atualizar():
-    item = forca_opcao('Qual item voce deseja atualizar?',acougue['Carnes'])
+    item = forca_opcao('Qual item voce deseja atualizar?\n->',acougue['Carnes'])
     indice_item = indices[item]
     keys = list(acougue.keys())
     keys.pop(0)
@@ -51,11 +64,79 @@ def atualizar():
     return
 
 
+def verifica_numero(msg):
+    num = input(msg)
+    while not num.isnumeric():
+        num = input(msg)
+    return int(num)
+
+
+def comprar():
+    item = forca_opcao('Qual item você quer comprar?\n->', acougue['Carnes'])
+    indice_item = indices[item]
+    for key in acougue.keys():
+        print(f'{key}: {acougue[key][indice_item]}')
+    continuar = forca_opcao('Você quer levar?',['sim', 'nao'])
+    if continuar == 'nao':
+        return
+    qtd = verifica_numero(f'Quantos kg de {item}? \n->')
+    if qtd <= acougue['estoque'][indice_item]:
+        acougue['estoque'][indice_item] -= qtd
+        carrinho['Valor Total'] += qtd*acougue['Preço/kg'][indice_item]
+        if item not in carrinho['Itens'].keys():
+            carrinho['Itens'][item] = qtd
+        else:
+            carrinho['Itens'][item] += qtd
+    else:
+        print(f'Só há {acougue['estoque'][indice_item]} kg no estoque!')
+        comprar()
+
+
 indices = cria_indices()
-atualizar()
+carrinho = {
+    'Endereço' : {
+        'Rua' : '',
+        'Bairro': '',
+        'Nª' : '',
+        'Cep' : ''
+    },
+    'Itens' : {},
+    'Valor Total' : 0
+}
+print('Bem vindo à Açougueria Agnelo!!!')
+usuario = forca_opcao('Você é',['cliente','funcionario'])
+while True:
+    if usuario == 'funcionario':
+        operacao = forca_opcao('Qual operação será realizada?',['cadastrar','remover','atualizar'])
+        if operacao == 'cadastrar':
+            add_prod()
+        elif operacao == 'remover':
+            rem_prod()
+        else:
+            atualizar()
+        continuar = forca_opcao('Você deseja realizar outras operações?',['sim','nao'])
+        if continuar == 'nao':
+            break
+        else:
+            comprar()
+            encerrar = forca_opcao('Encerrar a compra ou ver mais itens?',['encerrar', 'continuar'])
+            if encerrar == 'encerrar':
+                print(carrinho)
+                break
+
+
+'''for key in carrinho['Endereço'].keys():
+    info = input(f'Diga o/z {key}: \n->')
+    carrinho['Endereço'][key] = info'''
+
+
+comprar()
+print(carrinho)
+
+'''atualizar()
 print('-'*20)
 add_prod()
 print('-'*20)
 rem_prod()
 print('-'*20)
-print(pd.DataFrame(acougue))
+print(pd.DataFrame(acougue))'''
