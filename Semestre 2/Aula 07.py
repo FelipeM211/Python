b739d313fdb59da1fb82c53e21e7175fa551edc3
Felipe Balbino Murad <felipebmurad@gmail.com>
2025-10-16 11:33:37 -0300

    Update Aula 07.py

--- Semestre 2/Aula 07.py
+++ Semestre 2/Aula 07.py
@@ -1,6 +1,3 @@
-lista = [5,6,2,3,4]
-
-
 def indice_menor(lista):
     indice = 0
     for i in range(len(lista)):
@@ -9,14 +6,25 @@ def indice_menor(lista):
     return indice
 
 
-def selection_sort(lista):
-    aux = 0
+'''def selection_sort(lista):
+    ordenada = []
     while lista:
         local_menor = indice_menor(lista)
         menor = lista.pop(local_menor)
         ordenada.append(menor)
     return ordenada
+'''
 
+
+def selection_sort(lista):
+    for i in range(len(lista)):
+        local_menor = indice_menor(lista[i:]) + i
+        aux = lista[i]
+        lista[i] = lista[local_menor]
+        lista[local_menor] = aux
+    return lista
+
+
+lista = [3, 1, 5, 6, 2, 4, 7]
+lista = selection_sort(lista)
 print(lista)
-ordenada = selection_sort(lista)
-print(ordenada)
